4ea7c5daf7d245a80d719b886f0a1e090973170a
Andreas Almen <andreas.almen@live.com>
2024-08-27 09:19:40 +0000

    add function to delete post and edit posts via MoreDropdown

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -2,9 +2,10 @@ import React from "react";
 import styles from "../../styles/Post.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { axiosRes, axiosReq } from "../../api/axiosDefaults";
+import { MoreDropdown } from "../../components/MoreDropdown.";
 
 const Post = (props) => {
   const {
@@ -25,6 +26,20 @@ const Post = (props) => {
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+  const history = useHistory();
+
+  const handleEdit = () => {
+    history.push(`/posts/${id}/edit`);
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/posts/${id}/`);
+      history.goBack();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleLike = async () => {
     try {
@@ -35,7 +50,7 @@ const Post = (props) => {
       console.log(err);
     }
   };
-  
+
   const handleUnlike = async () => {
     try {
       await axiosRes.delete(`/likes/${like_id}/`);
@@ -45,8 +60,7 @@ const Post = (props) => {
       console.log(err);
     }
   };
-  
-  
+
   return (
     <Card className={styles.Post}>
       <Card.Body>
@@ -57,7 +71,12 @@ const Post = (props) => {
           </Link>
           <div className="d-flex align-items-center">
             <span>{updated_at}</span>
-            {is_owner && postPage && "..."}
+            {is_owner && postPage && (
+              <MoreDropdown
+                handleEdit={handleEdit}
+                handleDelete={handleDelete}
+              />
+            )}
           </div>
         </Media>
       </Card.Body>
